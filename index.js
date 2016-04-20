fa575946202925bb6b99a3fe34d3f8923e090176
Rando Christensen <rando.christensen@welocalize.com>
2016-04-20 02:50:46 -0700

    lib basics

---
+++ index.js
@@ -0,0 +1,19 @@
+'use strict';
+
+const sequential = require('./lib/sequential');
+const parallel = require('./lib/parallel');
+const batch = require('./lib/batch');
+const adaptCallback = require('./lib/adapt-callback');
+const delayed = require('./lib/delayed');
+const isPromise = require('./lib/is-promise');
+const toPromise = require('./lib/to-promise');
+
+module.exports = {
+  sequential,
+  parallel,
+  batch,
+  adaptCallback,
+  delayed,
+  isPromise,
+  toPromise
+};
